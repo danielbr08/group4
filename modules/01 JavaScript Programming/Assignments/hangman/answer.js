f7643b22b7aff12d9571f5ce4ac0d8153ec1e9fd
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-01-17 16:33:12 +0200

    resume...

--- modules/01 JavaScript Programming/Assignments/hangman/answer.js
+++ modules/01 JavaScript Programming/Assignments/hangman/answer.js
@@ -1,77 +1,83 @@
-// const phrase = 'Foo bar baz';
+const phrase1 = 'Foo bar baz';
+const phrase2 = 'Fow dal guy';
+const phrase3 = 'key ley nam';
+
+const phrases = [phrase1,phrase2,phrase3];
+// var phraseRandom = getPharseRandom(phrases);
+// console.log(phraseRandom);
+
 // const letters = ['a', 'f'];
-// const finds = getFindsStr(phrase, letters);
-// console.log(finds);
 
-// var getPharseRandom = function(phrases){
-//     randIndex = Math.floor((Math.random() * phrases.length) + 1);
-//     return phrases[randIndex];
-// }
+// const finds = getFindsStr(phraseRandom.split(''), letters);
+// //console.log(finds);
 
-// var isWon = function(phrase, letters){
-//     return (letters.every(function(value) { return phrase.indexof(value) !== -1}));
-// }
-
-// var getFindsStr = function(phrase, letters){
-//     var foundedLetters = '';
-//     phrase.forEach(function(element,index) {
-//         if(letters.indexof(element,index) != -1){
-//             foundedLetters += element;
-//         }
-//         else{
-//             foundedLetters += '_';
-//         }
-
-//     }, this);
-//     return foundedLetters;
-// }
-
-// var isInPhrase = function(phrase, letter){
-//     return phrase.some(item=> item === letter);
-// }
-
-// var isLetterAlreadyFound = function(letterToAdd,foundedLetters){
-//     if(foundedLetters.indexof(letterToAdd) === -1){
-//         return flase;
-//     return true;
-// }
-
-// var runGame = function(phrases){
-//     var life = 10,findsStr = null,letter ='',foundedLetters = new Array();
-//     var phrase = getPharseRandom(phrases);// return array of chars
-//     While(life > 0 && !isWon(phrase, letters)){
-//         console.log("Remaining life are:",life);
-//         findsStr = getFindsStr(phrase, letters)
-//         console.log(findsStr);
-//         letter = prompt("Please choose a letter");;//Get new letter from user
-//         if(!isInPhrase(phrase, letter)){
-//             life--;
-//         }
-//         else{
-//             if(!isLetterAlreadyFound(letter)){
-//                 foundedLetters.push(letter);
-//             }
-//         }
-//     }
-//     if (life === 0){
-//         console.log("You lost!");
-//     }
-//     else{
-//         console.log("You won!");
-//     }
-// }
-
-var getUserInput = function(){
-    const stdin = process.openStdin()
-    process.stdout.write('Please choose a letter: ')
-    var letter = '';
-    stdin.addListener('data', text => {
-       letter = text.toString().trim()
-      stdin.pause() // stop reading
-    })
-    return letter;
+function getPharseRandom(phrases){
+    randIndex = Math.floor((Math.random() * phrases.length));
+    return phrases[randIndex];
 }
 
-//var letter = prompt("Please choose a letter");;//Get new letter from user
-var i =getUserInput();
-console.log(i + 'o');
+function isWon(phrase, letters){
+    return (letters.every(function(value) { return phrase.indexof(value) !== -1}));
+}
+
+function isInPhrase(phrase, letter){
+    return phrase.some(item=> item === letter);
+}
+
+function isLetterAlreadyFound(letterToAdd,foundedLetters){
+    if(foundedLetters.indexOf(letterToAdd) === -1){
+        return flase;
+    }
+    return true;
+};
+
+function getFindsStr(phrase, letters){
+  var foundedLetters ='';
+  phrase.forEach(function(element) {
+      if(element === ' '){
+        foundedLetters += element + element + element;
+      }
+      else if(letters.indexOf(element) !== -1){
+          foundedLetters += element + ' ' ;
+      }
+      else{
+          foundedLetters += '_' + ' ';
+      }
+
+  }, this);
+  return foundedLetters;
+}
+
+function runGame(phrases){
+    var life = 10,findsStr = null,letter ='',foundedLetters = new Array();
+    var phrase = getPharseRandom(phrases).split('');// return array of chars
+    console.log(phrase);
+    while( life > 0 && !isWon(phrase, foundedLetters)){
+        console.log("Remaining life are:",life);
+        findsStr = getFindsStr(phrase, letters);
+        console.log(findsStr);
+        letter = getUserInput("Please choose a letter");//Get new letter from user
+        if(!isInPhrase(phrase, letter)){
+            life--;
+        }
+        else{
+            if(!isLetterAlreadyFound(letter)){
+                foundedLetters.push(letter);
+            }
+        }
+    }
+    if (life === 0){
+        console.log("You lost!");
+    }
+    else{
+        console.log("You won!");
+    }
+};
+
+function getUserInput(message,length){
+  var readline = require('readline-sync');
+  var input = readline.question(message);
+  return input;
+}
+
+runGame(phrases);
